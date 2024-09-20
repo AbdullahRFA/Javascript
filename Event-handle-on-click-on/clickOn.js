e589adf48409638fd8a0514e06fc4a54353441a5
AbdullahRFA <shakibrybmn@gmail.com>
2024-09-21 00:10:36 +0600

    Event handle as onclick method implemented

---
+++ Event-handle-on-click-on/clickOn.js
@@ -0,0 +1,13 @@
+function button1(){
+    document.querySelector("#btn1").innerHTML="Clicked";
+    alert("This Is button 1");
+}
+function details(){
+    document.querySelector(".para").innerHTML="I'm button 2";
+}
+function cat(){
+    document.querySelector("#img1").src="https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg";
+}
+function rat(){
+    document.querySelector("#img1").src="https://t3.ftcdn.net/jpg/00/68/43/12/360_F_68431244_BXVWMQPEJRxONCAru4jjspScjOzKIx5K.jpg";
+}
